ab1ba4e25d3d4dad3e871c776ef420c3798e87db
Jesse-DeJong <jdoceau@gmail.com>
2022-06-22 10:15:21 +1000

    refactored projects page to generate from a map of a JSON data file rather than being individually instantiated

---
+++ src/data/projectsData.js
@@ -0,0 +1,28 @@
+const projectsData = [
+    {
+        title: "Portfolio",
+        description: "A portfolio to introduce myself and my work to others.",
+        repo: "https://github.com/Jesse-DeJong/portfolio-latvia",
+        live: "https://jesse-dejong.github.io/portfolio-latvia"
+    },
+    {
+        title: "Morse Code Translator",
+        description: "A simple webhosted conversion tool to translate between Morse Code and English",
+        repo: "https://github.com/Jesse-DeJong/morse-code-translator",
+        live: "https://jesse-dejong.github.io/morse-code-translator/"
+    },
+    {
+        title: "EXAMPLE",
+        description: "A portfolio to introduce myself and my work to others.",
+        repo: "https://github.com/Jesse-DeJong/portfolio-latvia",
+        live: "https://jesse-dejong.github.io/portfolio-latvia"
+    },
+    {
+        title: "EXAMPLE",
+        description: "A simple webhosted conversion tool to translate between Morse Code and English",
+        repo: "https://github.com/Jesse-DeJong/morse-code-translator",
+        live: "https://jesse-dejong.github.io/morse-code-translator/"
+    }
+];
+
+export default projectsData;
